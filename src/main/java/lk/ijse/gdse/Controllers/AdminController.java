6f79ebae379364d1123a7c66a48005b22842f330
Paboda Sanduni <sandunikhitigama@gmail.com>
2024-11-03 00:58:00 +0530

    Commit

---
+++ src/main/java/lk/ijse/gdse/Controllers/AdminController.java
@@ -0,0 +1,4 @@
+package lk.ijse.gdse.Controllers;
+
+public class AdminController {
+}
---
+++ src/main/java/lk/ijse/gdse/Controllers/DashBoardController.java
@@ -0,0 +1,57 @@
+package lk.ijse.gdse.Controllers;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+
+public class DashBoardController {
+
+    @FXML
+    private Button btnCourse;
+
+    @FXML
+    private Button btnLogout;
+
+    @FXML
+    private Button btnPayment;
+
+    @FXML
+    private Button btnStudent;
+
+    @FXML
+    private Button btnStudentRegister;
+
+    @FXML
+    private Button btnUsers;
+
+    @FXML
+    void btnCourseOnAction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void btnLogoutOnAction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void btnPaymentOnAction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void btnStudentOnAction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void btnStudentRegisterOnAction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void btnUsersOnAction(ActionEvent event) {
+
+    }
+
+}
